f11188aed624ba05cb829c1f836b89b13406e117
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-12-16 04:10:20 -0800

    how to increase test set Specificity from 0.2727??

--- src/2018-12-15_xgboost-predicting-credit-default.R
+++ src/2018-12-15_xgboost-predicting-credit-default.R
@@ -12,6 +12,7 @@ library(tidyverse)
 library(caret)
 library(pROC)
 library(ISLR)
+library(Matrix)
 
 help(package = "xgboost")
 
@@ -20,8 +21,116 @@ help(package = "xgboost")
 data("Default")
 
 df1.default <- Default
-
 str(df1.default)
+
+
+# Create training data: 
+set.seed(1)
+train_index <- createDataPartition(df1.default$default, 
+                                   p = 0.8, 
+                                   list = FALSE, 
+                                   times = 1)
+
+df2.train <- df1.default[train_index, ]
+df3.test <- df1.default[-train_index, ]
+# nrow(df2.train) + nrow(df3.test)
+
+# sparse matrix of predictor values: 
+m1.sparse.train <- sparse.model.matrix(default ~ ., 
+                                 data = df2.train)[,-1]
+head(m1.sparse.train)
+
+# vector of response values: 
+train.target <- df2.train$default == "Yes"
+# table(train.target)
+
+
+# put predictor and response together: 
+m2.train.dmatrix <- xgb.DMatrix(data = m1.sparse.train, 
+                                label = as.numeric(train.target))
+
+
+# now get the test data ready: 
+m3.sparse.test <- sparse.model.matrix(default ~ ., 
+                                      data = df3.test)[,-1]
+
+head(m3.sparse.test)
+
+# vector of response values: 
+test.target <- df3.test$default == "Yes"
+table(test.target)
+
+
+
+# 2) Create xgboost model: -----------------------
+mod1 <- xgboost(data = m2.train.dmatrix, 
+                max_depth = 4, 
+                eta = 1, 
+                nrounds = 100,  
+                objective = "binary:logistic", 
+                verbose = TRUE)
+
+# for this problem, looks like train-error decreases
+# monotonically with increasing "nrounds" parameter. 
+# Maybe because this is simulated data? 
+
+# 3) predictions on the training data: ----------
+train.pred <- predict(mod1, 
+                      m1.sparse.train)
+
+# use threshold of 0.5 to convert to factor: 
+train.pred <- as.numeric(train.pred > 0.5)
+
+table(train.pred)  # predicted responses on training data 
+table(train.target)  # actual responses on training data 
+
+# 4) Confusion matrix on training data: -------
+confusionMatrix(factor(train.pred), 
+                factor(as.numeric(train.target)))
+
+# on train data, the model is doing really well. We expect
+# it to do worse on test data, but this is good enough that
+# we will proceed to predict without doing further tuning. 
+
+
+# 5) predict on the test set: --------
+test.pred <- predict(mod1,
+                     m3.sparse.test)
+
+# use threshold of 0.5 to convert to factor: 
+test.pred <- as.numeric(test.pred > 0.5)
+
+table(test.pred)  # predicted responses on test data 
+table(test.target)  # actual responses on test data 
+
+# 6) Confusion matrix on training data: -------
+confusionMatrix(factor(test.pred), 
+                factor(as.numeric(test.target)))
+
+# Sensitivity is still good, but specificity is quite bad: 
+# 0.2727
+# todo: how to increase the specificity?? 
+
+
+
+
+
+
+
+
+
+
+# 4) Alternative model development using cross-validation: ------------
+# Note: first do CV, then do one final prediction on the 
+# test data
+# reference: https://stats.stackexchange.com/questions/152907/how-do-you-use-test-data-set-after-cross-validation 
+
+
+
+
+
+
+
 
 
 
